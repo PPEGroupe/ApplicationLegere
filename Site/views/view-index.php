7e06c09ada9af04983f5915df1852ef041930d33
Zeymen <kevinpichon@hotmail.fr>
2016-03-17 23:33:50 +0100

    ADDFILE main.js IN Site/js.
    UPDATEFILES view-index.php IN Site/views;
    style.css IN Site/css.
    COMMENT : Ajout de la sélection d'offre dans la liste et la modal pour postuler.
    TODO : Ajouter dans la BDD.

--- Site/views/view-index.php
+++ Site/views/view-index.php
@@ -8,7 +8,7 @@
         <?php require '/views/view-header.php'; ?>
         
         <section class="container">
-            <table class="table table-striped" id="offers">
+            <table class="table" id="offers">
                 <thead>
                     <tr>
                         <th>Ref.</th>
@@ -25,7 +25,7 @@
                     {
                         foreach($offerList as $key => $offer)
                         { ?>
-                            <tr>
+                            <tr class="offer" id="offer<?php echo $offer->Identifier(); ?>">
                                 <td><?php echo $offer->Reference(); ?></td>
                                 <td><?php echo $offer->Title(); ?></td>
                                 <td><?php echo $offer->City(); ?></td>
@@ -42,5 +42,75 @@
                 </tbody>
             </table>
         </section>
+        
+        <div class="modal fade" id="postulateModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Postuler</h4>
+                    </div>
+                    <form action="" method="post" class="form-horizontal">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="title" class="col-sm-3 control-label">Titre</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="title" name="title" placeholder="Titre">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="website" class="col-sm-3 control-label">Site Internet</label>
+                                <div class="col-sm-9">
+                                    <input type="url" class="form-control" id="website" name="website" placeholder="Site internet">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="email" class="col-sm-3 control-label">E-mail</label>
+                                <div class="col-sm-9">
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="E-mail">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="phoneNumber" class="col-sm-3 control-label">N° Téléphone</label>
+                                <div class="col-sm-9">
+                                    <input type="tel" class="form-control" id="phoneNumber" name="phoneNumber" placeholder="N° Téléphone">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="fax" class="col-sm-3 control-label">Titre</label>
+                                <div class="col-sm-9">
+                                    <input type="tel" class="form-control" id="fax" name="fax" placeholder="Titre">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="address" class="col-sm-3 control-label">Adresse</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="address" name="adress" placeholder="Adresse postale">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="cv" class="col-sm-3 control-label">CV</label>
+                                <div class="col-sm-9">
+                                    <input type="file" class="form-control" id="file" name="file"/>
+                                </div>
+                            </div>
+                            
+                            <div class="modal-footer">
+                                <div class="form-group">
+                                    <div class="col-sm-offset-3 col-sm-9">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                                        <button type="submit" name="send" class="btn btn-warning">Postuler</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+        <script src="/js/jquery.js"></script>
+        <script src="/js/notify.js"></script>
+        <script src="/js/bootstrap.js"></script>
+        <script src="/js/main.js"></script>
     </body>
 </html>
