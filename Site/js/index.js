a70b6cb9b8344f51d61a5a158e27cff378e4548e
Bouzerrara <rad1-ste@hotmail.com>
2016-04-02 19:21:01 +0200

    UPDATEFILES : index.js IN Site/js, view-index.php IN Site/views.
    COMMENT : Ajout carte .
    ERROR : Probléme d'affichage.

--- Site/js/index.js
+++ Site/js/index.js
@@ -81,6 +81,29 @@ function InitializeMoreDetails() {
                 $('#detailsModal #jobQuantity').html(offer['JobQuantity']);
                 $('#detailsModal #jobDescription').html(offer['JobDescription']);
                 $('#detailsModal #profileDescription').html(offer['ProfileDescription']);
+                var longitude = offer['Longitude'];
+                var latitude  = offer['Latitude'];
+                console.log(offer);
+                console.log(longitude);
+                console.log(latitude);
+                var latlng = new google.maps.LatLng(longitude, latitude);
+                //objet contenant des propriétés avec des identificateurs prédéfinis dans Google Maps permettant
+	            //de définir des options d'affichage de notre carte
+                var options = {
+	            	center: latlng,
+	            	zoom: 19,
+	            	mapTypeId: google.maps.MapTypeId.ROADMAP
+	            };
+                //constructeur de la carte qui prend en paramêtre le conteneur HTML
+	            //dans lequel la carte doit s'afficher et les options
+                var carte = new google.maps.Map(document.getElementById("carte"), options);
+                
+                //création du marqueur
+                var marqueur = new google.maps.Marker({
+                    position: new google.maps.LatLng(longitude, latitude),
+                    map: carte
+                });
+                
                 $('#detailsModal').modal('show');
             },
             'json'
@@ -91,3 +114,4 @@ function InitializeMoreDetails() {
         });
     });
 }
+
