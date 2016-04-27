93fe93c2d4a9d94357a2226b74a9207546d9ff5a
Zeymen <kevinpichon@hotmail.fr>
2016-04-27 17:30:16 +0200

    UPDATEFILE view-head.php IN Site/views

--- Site/views/view-head.php
+++ Site/views/view-head.php
@@ -1,6 +1,9 @@
 <meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 <link rel="stylesheet" href="css/style.css"/>
 <link rel="stylesheet" href="css/responsive.css"/>
 <link rel="stylesheet" href="css/bootstrap.css"/>
 <link rel="shortcut icon" type="image/x-icon" href="pictures/icon.png"/>
+
 <title>MegaCasting</title>
